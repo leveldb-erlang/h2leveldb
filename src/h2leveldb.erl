be0fa6fd9d6fe9bad8e5d9c0842aae783b99afad
Tatsuya Kawano <tatsuya@hibaridb.org>
2015-12-31 00:17:17 +0800

    Update copyright year (2016)

--- src/h2leveldb.erl
+++ src/h2leveldb.erl
@@ -1,6 +1,6 @@
 %%% The MIT License
 %%%
-%%% Copyright (C) 2014-2015 by Tatsuya Kawano <tatsuya@hibaridb.org>
+%%% Copyright (C) 2014-2016 by Tatsuya Kawano <tatsuya@hibaridb.org>
 %%%
 %%% Permission is hereby granted, free of charge, to any person obtaining a copy
 %%% of this software and associated documentation files (the "Software"), to deal
