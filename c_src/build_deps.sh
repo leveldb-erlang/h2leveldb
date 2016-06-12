df0ff6ff731f865cf2c1780767345d8c81f343a6
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-06-12 20:27:01 +0800

    gh09 - Failure to integrate h2leveldb with Mix
    Remove the warning message about $REBAR_DEPS_DIR.

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -118,12 +118,7 @@ case "$1" in
     *)
         # a workaround for a problem in Elixir Mix
         if [ ! -d $REBAR_DEPS_DIR/snappy/.git ]; then
-            ORIGINAL_REBAR_DEPS_DIR="$REBAR_DEPS_DIR"
             REBAR_DEPS_DIR="$BASEDIR/../.."
-            echo
-            echo "NOTE: Invalid \$REBAR_DEPS_DIR: '${ORIGINAL_REBAR_DEPS_DIR}'"
-            echo "Falling-back to: '${REBAR_DEPS_DIR}'"
-            echo
         fi
 
         # snappy
